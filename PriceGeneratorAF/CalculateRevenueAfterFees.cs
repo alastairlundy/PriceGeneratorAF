1fa33e1d9cee56cfe15ed2aee2c0111c78fb93f5
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2023-12-20 10:17:20 +0000

    clarify creator of project

--- PriceGeneratorAF/CalculateRevenueAfterFees.cs
+++ PriceGeneratorAF/CalculateRevenueAfterFees.cs
@@ -8,7 +8,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Invoiceron.PriceGeneratorAF;
+namespace AlastairLundy.PriceGeneratorAF;
 
 public static class CalculateRevenueAfterFees
 {
